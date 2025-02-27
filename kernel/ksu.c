e539eb7fdb28c501d269d64f5d0428d2705d84fb
selfmusing <mirandamehek@gmail.com>
2025-02-27 15:41:49 +0530

    KSU: Bring Back NON-GKI support & prepare for sucompat backporting
    Revert "kernel: remove unused CONFIG guard becuase GKI kernel enable kprobe by default"
    
    This reverts commit 500ff9bcbca59ed6d292aaf24f57713c808bef74.
    
    Revert "kernel: Allow to re-enable sucompat"
    
    This reverts commit 4593ae81c78998dffbc81291eac15726a273a538.
    
    Revert "[1.0] Drop Non-GKI Support (#1483)"
    
    This reverts commit 898e9d4f8ca9b2f46b0c6b36b80a872b5b88d899.

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -57,8 +57,12 @@ int __init kernelsu_init(void)
 
 	ksu_throne_tracker_init();
 
+#ifdef CONFIG_KPROBES
 	ksu_sucompat_init();
 	ksu_ksud_init();
+#else
+	pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
+#endif
 
 #ifdef MODULE
 #ifndef CONFIG_KSU_DEBUG
@@ -76,8 +80,10 @@ void kernelsu_exit(void)
 
 	destroy_workqueue(ksu_workqueue);
 
+#ifdef CONFIG_KPROBES
 	ksu_ksud_exit();
 	ksu_sucompat_exit();
+#endif
 
 	ksu_core_exit();
 }
@@ -88,4 +94,7 @@ module_exit(kernelsu_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
