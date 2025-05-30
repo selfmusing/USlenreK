60346762f14bb251c73a9f5aec843fe06faa132f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-30 17:34:04 +0800

    kernel: core_hook: intercept devpts via security_inode_permission LSM
    `ksu handles devpts with selinux lsm hook` - aviraxp
    
    - no, not yet, but yes we can, thats a good idea.
    
    This change tries to do that, so instead of hooking pts_unix98_lookup or
    devpts_get_priv, we just watch security_inode_permission, if its devpts,
    pass it along to the original handler.
    
    EDIT: define devpts super magic if its undefined
    - yeah I aint gonna include a conditional include of a header just for this
    - while we can just fully remove the macro and inline, readability loss is bad
    
    Tested-by: alternoegraha <noegrahachan@gmail.com>
    Tested-by: AzyrRuthless <132790292+AzyrRuthless@users.noreply.github.com>
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -617,6 +617,22 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 	return 0;
 }
 
+#ifndef DEVPTS_SUPER_MAGIC
+#define DEVPTS_SUPER_MAGIC	0x1cd1
+#endif
+
+extern int ksu_handle_devpts(struct inode *inode); // sucompat.c
+
+int ksu_inode_permission(struct inode *inode, int mask)
+{
+	if (inode && inode->i_sb 
+		&& unlikely(inode->i_sb->s_magic == DEVPTS_SUPER_MAGIC)) {
+		//pr_info("%s: handling devpts for: %s \n", __func__, current->comm);
+		ksu_handle_devpts(inode);
+	}
+	return 0;
+}
+
 static int ksu_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 			  unsigned long arg4, unsigned long arg5)
 {
@@ -656,6 +672,7 @@ static struct security_hook_list ksu_hooks[] = {
 	LSM_HOOK_INIT(task_prctl, ksu_task_prctl),
 	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
 	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
+	LSM_HOOK_INIT(inode_permission, ksu_inode_permission),
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
 	LSM_HOOK_INIT(key_permission, ksu_key_permission)
 #endif
