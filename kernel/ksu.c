7f813836ae2a564c9f4235814b9ac870e0a627c2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-19 19:49:17 +0800

    kernel: extras: SQUASH: avc log spoofing impl
    Co-Authored-By: KOWX712 <185127155+kowx712@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -71,6 +71,10 @@
 #include "kp_ksud.c"
 #endif
 
+#ifdef CONFIG_KSU_EXTRAS
+#include "extras.c"
+#endif
+
 // __weak fn's
 #include "kernel_compat.c"
 
@@ -117,6 +121,10 @@ int __init kernelsu_init(void)
 
 #ifdef CONFIG_KSU_KPROBES_KSUD
 	kp_ksud_init();
+#endif
+
+#ifdef CONFIG_KSU_EXTRAS
+	ksu_avc_spoof_init(); // so the feature is registered
 #endif
 
 	return 0;
