871621536be65831c3a40d62fec35a063e2db336
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-19 19:49:17 +0800

    kernel: extras: SQUASH: avc log spoofing impl
    Co-Authored-By: KOWX712 <185127155+kowx712@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -63,6 +63,10 @@
 #include "syscall_table_hook.c"
 #endif
 
+#ifdef CONFIG_KSU_EXTRAS
+#include "extras.c"
+#endif
+
 struct cred* ksu_cred;
 
 extern void ksu_supercalls_init();
@@ -102,6 +106,10 @@ int __init kernelsu_init(void)
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 	ksu_syscall_table_hook_init();
+#endif
+
+#ifdef CONFIG_KSU_EXTRAS
+	ksu_avc_spoof_init(); // so the feature is registered
 #endif
 
 	return 0;
