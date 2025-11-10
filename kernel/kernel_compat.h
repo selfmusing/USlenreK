347544d301786b2c0e3034cbc9fc98a5c9d52e1c
Wang Han <18079988+aviraxp@users.noreply.github.com>
2025-11-10 20:45:15 +0800

    kernel: supercalls: backport: "Use task work to install fd"
    There are many fd related functions that can sleep, so we have no choice
    but move operations to task work. Also close fd when copy_to_user fails.
    
    upstream: https://github.com/tiann/KernelSU/commit/c56a7068c25b1594d6fa6d84accdd40d287c7d9c
    
    --
    since this repo will also offer sys_reboot via kprobe, this is kind of a must have.
    can't really sleep inside kprobe context.
    
    as for locking this to 3.5, older does not really have task work infra
    so if its called inside a kprobe, well, its illegal, but it'll work.
    we abuse UB in C all day everyday anyway.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -29,4 +29,25 @@ static inline void ksu_kvfree(void *buf)
 #define kvfree ksu_kvfree
 #endif
 
+// for supercalls.c fd install tw
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 7, 0)
+#ifndef TWA_RESUME
+#define TWA_RESUME 1
+#endif
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 7, 0)
+#define close_fd sys_close
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0)
+#include <linux/fdtable.h>
+__weak int close_fd(unsigned fd)
+{
+	// this is ksys_close, but that shit is inline
+	// its problematic to cascade a weak symbol for it
+	return __close_fd(current->files, fd);
+}
+#endif
+
 #endif
