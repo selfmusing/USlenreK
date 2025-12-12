a90de37aec6b7f49dee684457bb5c6ff6b5cfb5b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-12 22:15:55 +0800

    kernel: tiny_sulog: basic ringbuffer, timestamped su log
    This is a very basic ringbuffer implementation.
    on this one we keep 8 bytes per entry and 250 entries max.
    
    This logs escalation access about: who did it, what happened and when did
    it happen.
    
    userspace can deduce as we also send next index to it. which makes it so
    that this is also the oldest entry. next -1 is the latest entry.
    
    v2: timestamped version
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -102,7 +102,8 @@ static inline void kernel_execve_escape_ksud(void *filename_ptr) { } // no-op
 
 static noinline int ksu_sucompat_user_common(const char __user **filename_user,
 				const char *syscall_name,
-				const bool escalate)
+				const bool escalate,
+				const uint8_t sym)
 {
 	const char su[] = SU_PATH;
 
@@ -115,6 +116,8 @@ static noinline int ksu_sucompat_user_common(const char __user **filename_user,
 
 	if (memcmp(path, su, sizeof(su)))
 		return 0;
+
+	write_sulog(sym);
 
 	if (!escalate)
 		goto no_escalate;
@@ -150,7 +153,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "faccessat", false);
+	return ksu_sucompat_user_common(filename_user, "faccessat", false, 'a');
 }
 
 // sys_newfstatat, sys_fstat64
@@ -159,7 +162,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "newfstatat", false);
+	return ksu_sucompat_user_common(filename_user, "newfstatat", false, 's');
 }
 
 // sys_execve, compat_sys_execve
@@ -176,14 +179,16 @@ static int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "sys_execve", true);
+	return ksu_sucompat_user_common(filename_user, "sys_execve", true, 'x');
 }
 
-static noinline int ksu_sucompat_kernel_common(void *filename_ptr, const char *function_name, bool escalate)
+static noinline int ksu_sucompat_kernel_common(void *filename_ptr, const char *function_name, bool escalate, const uint8_t sym)
 {
 
 	if (likely(memcmp(filename_ptr, SU_PATH, sizeof(SU_PATH))))
 		return 0;
+
+	write_sulog(sym);
 
 	if (!escalate)
 		goto no_escalate;
@@ -226,7 +231,7 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, voi
 	if (!is_su_allowed((const void **)filename_ptr))
 		return 0;
 
-	return ksu_sucompat_kernel_common((void *)(*filename_ptr)->name, "do_execveat_common", true);
+	return ksu_sucompat_kernel_common((void *)(*filename_ptr)->name, "do_execveat_common", true, 'x');
 }
 int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
@@ -247,7 +252,7 @@ int ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp
 	if (!is_su_allowed((const void **)filename_ptr))
 		return 0;
 
-	return ksu_sucompat_kernel_common((void *)*filename_ptr, "do_execve_common", true);
+	return ksu_sucompat_kernel_common((void *)*filename_ptr, "do_execve_common", true, 'x');
 }
 #endif
 
