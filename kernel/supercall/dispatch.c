2ec8d0b5c08907108c47503909b4ba4c791de804
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-28 14:13:55 +0800

    kernel: supercalls: expose ksuver override

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -10,6 +10,8 @@ static int do_grant_root(void __user *arg)
 	return 0;
 }
 
+static uint32_t ksuver_override = 0;
+
 static int do_get_info(void __user *arg)
 {
 	struct ksu_get_info_cmd cmd = {.version = KERNEL_SU_VERSION, .flags = 0};
@@ -20,6 +22,8 @@ static int do_get_info(void __user *arg)
 	}
 	cmd.features = KSU_FEATURE_MAX;
 
+	if (ksuver_override)
+		cmd.version = ksuver_override;
 
 	if (copy_to_user(arg, &cmd, sizeof(cmd))) {
 		pr_err("get_version: copy_to_user failed\n");
