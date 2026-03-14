a250f38e5d2638726bbec285a38df25b67f151f4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-14 18:15:32 +0800

    kernel: prepare for legacy support
    Since upstream has way different methods, via kprobes/kretprobes/LKM/tracepoints,
    cleaning up support for unsupportable code is kind of a must. This simplifies porting
    downstream changes, restoring old components, debloat, and makes it easier to maintain
    downstream, e.g. avoiding excessive use of conditionals (ifdef hell).
    
    Theres now massive code divergence so this has to be done.
    We will restore compatibility to upstream with the next commits.
    
    This also restores compatibility code needed for legacy that was removed upstream.
    
    Restores LSM hooks:
    - inode_rename - replacss pkg_observer.c
    - task_fix_setuid - replaces setuid_hook.c + kernel_umount.c
    
    Restores workqueue'd allowlist handling dropped at bfb767b, 9060e61
    Restores -Wno-implicit-function-declaration dropped at d3a09fa
    Restores old core_hook infrastructure. breaking it up to setuid + kernel_umount is just more complexity here.
    Restores an ifdef on arch.h, adds in-32-bit arm support.
    Restores Makefile instead of Kbuild - TODO: review this later for UL
    Restore needed rules for init's execution of ksud - removed on 6e918fc, pr 3031
    
    smaller changes and cleanups
    	sucompat: ksu_handle_stat(), remove dead ifdef.
    	LINUX_VERSION_CODE / KERNEL_VERSION, ksu.c
    		- reported by Sinclair19
    		- fix by including version.h
    	fatal_signal_pending, ksud.c
    		- add compat by including sched.h or sched/signal.h conditionally
    		- ref: https://github.com/torvalds/linux/commit/2a1f062a4acf0be50516ceece92a7182a173d55a
    	selinux_state.ss, core_hook.c
    		- remove rcu_dereference use
    		- ref: tiann#2695
    	seccomp.filter_count, core_hook.c
    		- reset this only for 5.9 and up as it only exists there
    		- ref: tiann#2708, https://github.com/gregkh/linux/commit/c818c03b661cd769e035e41673d5543ba2ebda64
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.h
+++ kernel/core_hook.h
@@ -1,15 +1,13 @@
-#ifndef __KSU_H_KERNEL_UMOUNT
-#define __KSU_H_KERNEL_UMOUNT
+#ifndef __KSU_H_KSU_CORE
+#define __KSU_H_KSU_CORE
 
-#include <linux/types.h>
+#include <linux/init.h>
 #include <linux/list.h>
 #include <linux/rwsem.h>
 
-void ksu_kernel_umount_init(void);
-void ksu_kernel_umount_exit(void);
+void __init ksu_core_init(void);
 
-// Handler function to be called from setresuid hook
-int ksu_handle_umount(uid_t old_uid, uid_t new_uid);
+void escape_with_root_profile(void);
 
 // for the umount list
 struct mount_entry {
