yapixel/KernelSU

kernel: ksud: d_is_reg to S_ISREG
d_is_reg requires 4.0
 - https://github.com/torvalds/linux/commit/e36cb0b89ce20b4f8786a57e8a6bc8476f577650
S_ISREG is still there on 6.15 so I do NOT see any issues forcing it for all.

Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

kernel/ksud.c
@@ -282,7 +282,7 @@ int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 		return 0;
 	}
 
-	if (!d_is_reg(file->f_path.dentry)) {
+	if (!S_ISREG(file->f_path.dentry->d_inode->i_mode)) {
 		return 0;
 	}
 

selfmusing/USlenreK

kernel/ksud.c
@@ -310,7 +310,7 @@ static bool is_init_rc(struct file *fp)
 		return false;
 	}
 
-	if (!d_is_reg(fp->f_path.dentry)) {
+	if (!S_ISREG(fp->f_path.dentry->d_inode->i_mode)) {
 		return false;
 	}
 