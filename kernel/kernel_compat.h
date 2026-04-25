86851e4eb7c321a791c90c7bbaedb8b806ed0bf8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-25 09:16:38 +0800

    kernel: pkg_observer: initialize for downstream
    reuses upstream:
    https://github.com/tiann/KernelSU/blob/v1.0.5/kernel/core_hook.c
    
    in this one, we try to cache d_inode to avoid dentry_path_raw for fast path.
    some time is spent on dentry_path_raw and it is locking.
    we can cache .data/system's d_inode and if its the same,
    we skip dentry_path_raw
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -1,4 +1,8 @@
 #ifndef __KSU_H_KERNEL_COMPAT
 #define __KSU_H_KERNEL_COMPAT
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 0, 0)
+#define d_inode(dentry) ((dentry)->d_inode)
+#endif
+
 #endif // __KSU_H_KERNEL_COMPAT
